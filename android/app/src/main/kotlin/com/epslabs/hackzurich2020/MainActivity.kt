3e1cfb9933628039c3dd415f1ffc9717ea23e370
David Pacassi Torrico <david@eps-labs.com>
2020-09-19 01:51:40 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/epslabs/hackzurich2020/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.epslabs.hackzurich2020
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

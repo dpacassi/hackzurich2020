917445735dbce1e1275dcf77dd4d7f0a5d0396e5
David Pacassi Torrico <david@eps-labs.com>
2020-09-19 03:11:19 +0200

    Add a very basic ean13 barcode scanner

---
+++ lib/widgets/CameraWidget.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:fast_qr_reader_view/fast_qr_reader_view.dart';
+
+
+class CameraWidget extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  CameraWidget({ this.cameras });
+
+  @override
+  _CameraWidgetState createState() => new _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  QRReaderController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new QRReaderController(widget.cameras[0], ResolutionPreset.high, [CodeFormat.ean13], (dynamic value){
+      print(value); // the result!
+      // ... do something
+      // wait 3 seconds then start scanning again.
+      new Future.delayed(const Duration(seconds: 3), controller.startScanning);
+    });
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+      controller.startScanning();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return new Container();
+    }
+
+    return new AspectRatio(
+        aspectRatio:
+        //controller.value.aspectRatio,
+        1.0,
+        child: new QRReaderPreview(controller)
+    );
+  }
+}
